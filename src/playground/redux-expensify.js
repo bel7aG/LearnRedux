1242721c0f0c27a84de4e5e9b2e805e4170aa43a
Belhassen <Belhassen.Gharsallah@isetrades.com>
2018-05-20 17:50:49 +0200

    wrapping up reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -31,17 +31,33 @@ const editExpense = (id, update) => ({
   update
 });
 
-// //SET_TEXT_FILTER
+//SET_TEXT_FILTER
 const setTextFilter = (text = '') => ({
   type: 'SET_TEXT_FILTER',
   text
 });
 
-// //SORT_BY_DATE
-// //SORT_BY_AMOUNT
-// //SORT_START_DATE
-// //SORT_END_DATE
+//SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT',
+});
 
+//SORT_BY_DATE
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
+});
+
+//SORT_START_DATE
+const startDate = (startDate = undefined) => ({
+  type: 'SORT_START_DATE',
+  startDate
+});
+
+//SORT_END_DATE
+const endDate = (endDate = undefined) => ({
+  type: 'SORT_END_DATE',
+  endDate
+});
 
 //Expense Reducer
 const expenseReducerDefaultState = [];
@@ -86,6 +102,26 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
         ...state,
         text: action.text
       };
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+        sortBy: 'amount'
+      };
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date'
+      };
+    case 'SORT_START_DATE':
+      return {
+        ...state,
+        startDate: action.startDate
+      };
+    case 'SORT_END_DATE':
+      return {
+        ...state,
+        endDate: action.endDate
+      };
     default:
       return state;
   }
@@ -114,7 +150,21 @@ store.dispatch(editExpense(expenseOne.expense.id, { amount: 1820 }));
 
 store.dispatch(setTextFilter('rent'));
 
-store.dispatch(setTextFilter(''));
+// store.dispatch(setTextFilter(''));
+
+store.dispatch(sortByAmount());
+
+store.dispatch(sortByDate());
+
+store.dispatch(startDate(222));
+store.dispatch(startDate());
+
+store.dispatch(endDate(3000));
+
+
+
+
+
 
 
 
